51b11330ed27cf73aa59d1037df22ef88b1436e5
Fellipe Ribeiro Martins <121965162+Fellipedev1@users.noreply.github.com>
2023-12-03 14:18:19 -0300

    excluir frase

--- server.js
+++ server.js
@@ -18,7 +18,9 @@ app.get('/api/data', (req, res) => {
 
     try {
       const jsonData = JSON.parse(data);
-      res.json(jsonData);
+      // Assuming that your data is an array, you can filter out the undesired sentence
+      const filteredData = jsonData.filter(item => item.Column1 !== "Lista de Medicamentos Similares e seus respectivos medicamentos de referência, conforme RDC 58/2014 Atualizada até 11/05/2020, conforme o Diário Oficial da União. Lista de Medicamentos Similares classificada por ordem alfabética do medicamento de referência");
+      res.json(filteredData);
     } catch (jsonError) {
       console.error('Erro ao analisar o JSON:', jsonError);
       res.status(500).json({ error: 'Erro ao analisar o JSON' });
